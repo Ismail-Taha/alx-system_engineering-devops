d3a911d6ea11817220c05dfa78b48dd5e14091d9
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-02-27 18:35:11 -0800

    use pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,24 @@
-i#automating my work using Puppet
+# Setup New Ubuntu server with nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-
-ensure => installed,
-
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file_line { 'install':
-
-ensure => 'present'
-
-path => /etc/nginx/sites-available/default',
-
-after => 'listen 80 default_server;',
-
-} line => 'rewrite ^/redirect_me https://www.youtube.com/ permanent;,
-
-file '/var/www/html/index.html':
-
-content => 'Hello world!',
-
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-service 'nginx':
+exec {'redirect_me':
+	command => 'sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
 
-ensure => running,
-
-require => Package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
